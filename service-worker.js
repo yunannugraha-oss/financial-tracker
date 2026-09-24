bb3ad4c1a8f02b13f5eaee8ca2c6483c7510fded
yunannugraha-oss <yunan.nugraha@gmail.com>
2026-09-25 00:17:41 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
